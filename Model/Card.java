90bdcd1884ea936bdf19d33847f5f681691f47c0
JoseAngel <Jangelgiron@gmail.com>
2023-12-01 10:42:47 +0100

    Esbozo modelo

--- Model/Card.java
+++ Model/Card.java
@@ -7,6 +7,7 @@ public class Card {
     private String suit;
 
 
+
     public Card(){
         this(0,"");
 
---
+++ Model/Deck.java
@@ -0,0 +1,39 @@
+package Model;
+
+public class Deck {
+
+
+    Card[] cards;
+
+
+
+    public static void createDeck(){
+
+
+    }
+
+    public static void shuflle(){
+
+
+    }
+
+    public static void removeCard(){
+
+
+    }
+    public static void getCard(){
+
+
+    }
+
+    public static int deckSize(){
+
+
+        return 0;
+    }
+
+
+
+
+
+}
