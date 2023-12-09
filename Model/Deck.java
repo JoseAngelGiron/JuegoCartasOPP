81ede2927ff024b580ffc437b01b4b6872aca395
JoseAngel <Jangelgiron@gmail.com>
2023-12-09 18:00:32 +0100

    Añadida función para dar una sola carta

--- Model/Deck.java
+++ Model/Deck.java
@@ -2,6 +2,7 @@ package Model;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Deck {
 
@@ -59,6 +60,7 @@ public class Deck {
     /**
      *
      * Esta función se encarga de repartir las 2 primeras cartas a cada jugador.
+     * Elimina las cartas del mazo que va pasando con la función de remove card
      *
      *
      * @param players recibe el arreglo de jugadores
@@ -68,12 +70,31 @@ public class Deck {
             for (Player player:players) {
                 Card[] card = player.getHand();
                 for(int i=0;i<=1;i++){
-                    Card cardToPass = searchCard();
+                    Card cardToPass = cards[searchCardPosition()];
+                    removeCard(searchCardPosition());
                     card[i] = cardToPass;
 
                 }
                 player.setHand(card);
             }
+    }
+
+    /**
+     * Esta función recibe un jugador y da una carta al jugador que se le pasa.
+     * Hace uso de la función nullPosition para averiguar la primera posición que apunta a nulo en la mano del jugador
+     * Hace uso de la función removeCard para eliminar la carta del mazo cuando se la damos al jugador.
+     * @param player recibe un jugador de jugadores
+     */
+    public void dealACard(Player player){
+
+            int nullPosition = firstNullPosition(player.getHand()); //Busca la primera posición nula en la mano del jugador
+            int cardPosition =  searchCardPosition();// Busca la posición de una carta aleatoria en el mazo
+            Card cardToDeal = cards[cardPosition]; // Cogemos la carta del mazo que vamos a asignar
+            player.getHand()[nullPosition] = cardToDeal; // Asigna dicha carta a la mano del jugador en la primera posición vacía que tiene
+            cards[cardPosition] = null;// Eliminamos la carta del mazo
+
+
+
     }
 
     /**
@@ -98,18 +119,19 @@ public class Deck {
     }
 
     /**
-     * Esta función devuelve la posición de una carta, la primera que encuentra, seleccionada aleatoriamente entre todas las del mazo
+     * Esta función devuelve una carta, la primera que encuentra, seleccionada aleatoriamente entre todas las del mazo.
+     * Si la posición de la carta seleccionada es nula (o sea, no hay nada), selecciona otra posición y por tanto, otra carta.
      * @return Devuelve la posicion de la carta encontrada en la posición que le hemos dado.
      */
-    public Card searchCard() {
+    public int searchCardPosition() {
         int cardPosition;
-        Card cardToReturn;
+        Card cardToEvaluate;
         do {
-            cardPosition = (int) (Math.random() * (52));
-            cardToReturn = cards[cardPosition];
+            cardPosition = (int) (Math.random() * (52)); // devuelve la posición de la carta
+            cardToEvaluate = cards[cardPosition]; // si esto es null, repite la operación
 
-        } while (cardToReturn == null);
-        return cardToReturn;
+        } while (cardToEvaluate == null);
+        return cardPosition;
     }
 
     /**
--- Model/Game.java
+++ Model/Game.java
@@ -2,149 +2,173 @@ package Model;
 
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Game {
+        public class Game {
+                //Atributos
+                private Player[] players;
+
+                Deck deck;
+
+                private boolean isBlackJack;
+
+                // Getter and Setters
+                public Player[] getPlayers() {
+                        return players;
+                }
+
+                public void setPlayers(Player[] players) {
+                        this.players = players;
+                }
+
+                public boolean isBlackJack() {
+                        return isBlackJack;
+                }
+
+                public void setBlackJack(boolean blackJack) {
+                        isBlackJack = blackJack;
+                }
 
 
-        private boolean isBlackJack;
+                @Override
+                public String toString() {
+                        return "Game{" +
+                                "players=" + Arrays.toString(players) +
+                                ", deck=" + deck +
+                                ", isBlackJack=" + isBlackJack +
+                                '}';
+                }
+
+                /**
+                 * Esta función se encarga de iniciar el juego.
+                 * Recibe funciones XXXX
+                 */
+                public void startGame(){
 
 
-        public boolean isBlackJack() {
-                return isBlackJack;
-        }
-
-        public void setBlackJack(boolean blackJack) {
-                isBlackJack = blackJack;
-        }
-
-        public void startGame(){
-
-        }
-        public void playDealerTurn(Player players){
-
-
-
-        }
-
+                }
+                public void playDealerTurn(Player players){
 
 
 
-        /**
-         * Esta función comprueba, tras la 2 cartas iniciales, las manos de los jugadores. Y si obtiene única combinación posible
-         * de BlackJack cambia el atributo del jugador de blackJack a True
-         * @param players Recibe un arreglo de jugadores, es decir, los jugadores que van a jugar el juego.
-         */
-        public void updateBlackJack(Player[] players){
-                int acu =0;
-                for(int i =0;i< players.length;i++) {
-                        Card[] cards = players[i].getHand();
-                        for (int j=0;j<cards.length && cards[j] !=null;j++) {
-                                if (cards[j] != null) {
-                                        acu += cards[j].getValue();
-                                        if(acu==11){
-                                                players[i].setBlackJack(true);
-                                                setBlackJack(true);
+                }
+
+                /**
+                 * Esta función comprueba, tras la 2 cartas iniciales, las manos de los jugadores. Y si obtiene única combinación posible
+                 * de BlackJack cambia el atributo del jugador de blackJack a True
+                 * @param players Recibe un arreglo de jugadores, es decir, los jugadores que van a jugar el juego.
+                 */
+                public void updateBlackJack(Player[] players){
+                        int acu =0;
+                        for(int i =0;i< players.length;i++) {
+                                Card[] cards = players[i].getHand();
+                                for (int j=0;j<cards.length && cards[j] !=null;j++) {
+                                        if (cards[j] != null) {
+                                                acu += cards[j].getValue();
+                                                if(acu==11){
+                                                        players[i].setBlackJack(true);
+                                                        setBlackJack(true);
+                                                }
+
                                         }
-
                                 }
                         }
+
                 }
 
-        }
-
-        /**
-         * Comprueba si más de un jugador tiene blackjack.
-         * @param players recibe un arreglo con todos los jugadores
-         * @return amountOfBlacks, la cantidad de blackjacks que se han dado
-         */
-        private int checkBlackJack(Player[] players) {
-                int amountOfBlacks=0;
-                for (Player player:players) {
-                        if(player.isBlackJack()){
-                                amountOfBlacks+=1;
+                /**
+                 * Comprueba si más de un jugador tiene blackjack.
+                 * @param players recibe un arreglo con todos los jugadores
+                 * @return amountOfBlacks, la cantidad de blackjacks que se han dado
+                 */
+                private int checkBlackJack(Player[] players) {
+                        int amountOfBlacks=0;
+                        for (Player player:players) {
+                                if(player.isBlackJack()){
+                                        amountOfBlacks+=1;
+                                }
                         }
-                }
-                return amountOfBlacks;
-        }
-
-        /**
-         * Comprueba si un jugador se ha pasado de 21 puntos, incluida la IA
-         * @param players Recibe el arreglo de jugadores.
-         */
-        public void checkBust(Player[] players){
-                for(Player player:players)
-                        player.setPlaying(player.getPoints()>21);
-                eliminatePlayer();
-        }
-
-        private void eliminatePlayer() {
-        }
-
-        /**
-         * Esta función se encarga de establecer los jugadores. Hace uso de la función askNames. Tanto esta función como askNames están sujetas a cambios
-         * @param numberOfPlayers recibe el número de jugadores
-         * @return devuelve un arreglo de jugadores, con la cantidad de jugadores que le hemos pasado.
-         */
-        public Player[] addPlayer ( int numberOfPlayers ){
-                String[] names = askNames(numberOfPlayers);
-                Player[] players = new Player[numberOfPlayers+1];
-
-                for (int i=0;i<players.length;i++){
-                        players[i]  = new Player(0,names[i], false, true);
+                        return amountOfBlacks;
                 }
 
-                return players;
-        }
-
-        /**
-         * Esta función se encarga de pedir nombres, y devolver un arreglo con todos los nombres.
-         * @param numberOfPlayers Recibe la cantidad de jugadores
-         * @return devuelve un arreglo de Strings con los jugadores
-         */
-        public String[] askNames (int numberOfPlayers){
-
-                Scanner teclado = new Scanner(System.in);
-                String name;
-                String[] names = new String[numberOfPlayers+1];
-                names[0] = "IA";
-                for (int i =1;i< names.length; i++) {
-                        name = teclado.nextLine();
-                        names[i] = name;
+                /**
+                 * Comprueba si un jugador se ha pasado de 21 puntos, incluida la IA
+                 * @param players Recibe el arreglo de jugadores.
+                 */
+                public void checkBust(Player[] players){
+                        for(Player player:players)
+                                player.setPlaying(player.getPoints()>21);
+                        eliminatePlayer();
                 }
 
+                private void eliminatePlayer() {
+                }
 
-                return names;
-        }
+                /**
+                 * Esta función se encarga de establecer los jugadores. Hace uso de la función askNames. Tanto esta función como askNames están sujetas a cambios
+                 * @param numberOfPlayers recibe el número de jugadores
+                 * @return devuelve un arreglo de jugadores, con la cantidad de jugadores que le hemos pasado.
+                 */
+                public Player[] addPlayer ( int numberOfPlayers ){
+                        String[] names = askNames(numberOfPlayers);
+                        Player[] players = new Player[numberOfPlayers+1];
 
-        public void showHands(Player[] players){
-                for (Player player:players){
-
-                        System.out.println(player.getName());
-                        System.out.println("Puntos: " + player.getPoints()+"\n");
-                        Card[] mano = player.getHand();
-                        for (int j=0;j< mano.length && mano[j]!=null ;j++){
-                                System.out.println(mano[j]);
+                        for (int i=0;i<players.length;i++){
+                                players[i]  = new Player(0,names[i], false, true);
                         }
-                        System.out.println();
+
+                        return players;
+                }
+
+                /**
+                 * Esta función se encarga de pedir nombres, y devolver un arreglo con todos los nombres.
+                 * @param numberOfPlayers Recibe la cantidad de jugadores
+                 * @return devuelve un arreglo de Strings con los jugadores
+                 */
+                public String[] askNames (int numberOfPlayers){
+
+                        Scanner teclado = new Scanner(System.in);
+                        String name;
+                        String[] names = new String[numberOfPlayers+1];
+                        names[0] = "IA";
+                        for (int i =1;i< names.length; i++) {
+                                name = teclado.nextLine();
+                                names[i] = name;
+                        }
+
+
+                        return names;
+                }
+
+                public void showHands(Player[] players){
+                        for (Player player:players){
+
+                                System.out.println(player.getName());
+                                System.out.println("Puntos: " + player.getPoints()+"\n");
+                                Card[] mano = player.getHand();
+                                for (int j=0;j< mano.length && mano[j]!=null ;j++){
+                                        System.out.println(mano[j]);
+                                }
+                                System.out.println();
+                        }
+
+
+                }
+
+                public void calculatePoints(Player[] players){
+                        for (Player player : players) {
+                            Card[] mano = player.getHand();
+                            int puntos = 0;
+                            for (int j = 0; j < mano.length && mano[j] != null; j++) {
+                                puntos += mano[j].getValue();
+                            }
+                            player.setPoints(puntos);
+                        }
+
+
                 }
 
 
-        }
-
-        public void calculatePoints(Player[] players){
-                for (Player player : players) {
-                    Card[] mano = player.getHand();
-                    int puntos = 0;
-                    for (int j = 0; j < mano.length && mano[j] != null; j++) {
-                        puntos += mano[j].getValue();
-                    }
-                    player.setPoints(puntos);
-                }
-
 
         }
-
-
-
-}
