731c0a213abf5ade1ba10baf0b642ecd82ac48ca
JoseAngel <Jangelgiron@gmail.com>
2023-12-09 22:36:51 +0100

    Mejoras sobre la vista

--- Controller/MainController.java
+++ Controller/MainController.java
@@ -1,25 +1,41 @@
 package Controller;
 
+import Model.Game;
 import View.Menu;
 
 public class MainController {
+
+    static Game game;
+
     public static void startApp(){
         int option =-1;
         do{
-            option = Menu.mainMenu();
-            mainController(option);
-        }while(option!=5);
+
+                option = Menu.mainMenu();
+                mainController(option);
+                if(option!=2)
+                    System.out.println("La opción seleccionada no esta contemplada, pruebe de nuevo por favor");
+
+
+        }while(option!=2);
 
     }
 
-    public static void mainController(int option){
+    public static void mainController(int option) {
 
+
+        switch (option) {
+            case 1:
+                game = new Game();
+                game.startGame();
+                break;
+            case 2:
+
+                break;
+
+
+        }
 
 
     }
-
-
-
-
-
 }
--- View/IO.java
+++ View/IO.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 
 public class IO {
 
-        public static int readInt(String msg){
+        public static int readInt(String msg) {
             Scanner teclado = new Scanner(System.in);
-            System.out.print(msg+": ");
-            return teclado.nextInt();
+            System.out.print(msg + ": ");
+            int opcionteclado = -1;
+            try {
+                opcionteclado = teclado.nextInt();
+            } catch (Exception e) {
+                teclado.nextLine();
+                System.out.println("La opción introducida no es valida, pruebe de nuevo");
+            }
+            return opcionteclado;
         }
         public static String readString(String msg){
             Scanner teclado = new Scanner(System.in);
